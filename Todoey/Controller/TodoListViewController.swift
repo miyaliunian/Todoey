ab6f61904a213db804e17d597c29aa6df8c9b339
wufei <u9W88z6254>
2018-11-06 10:17:42 +0800

    conver tp MVC design pattern

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -11,18 +11,24 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     //MARK:- 属性
-    var itemArray = ["Find Mike","Buy Eggos","Destory Demogorgon"]
+    var itemArray : [Item] = [Item]()
     
+    let defaults = UserDefaults.standard
     
     //MARK:- 系统回调
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        
+        
+        if let items  = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
     }
 
     // MARK: - Table view data source
@@ -37,18 +43,18 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
  
     //MARK:- tabe lView delegate methods
     //ToDo: - 当前选中行
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
-           tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+           itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //强制tableView再次调用它的dataSourceMethods
+        tableView.reloadData()
         //正常状态下 选中tableView时选中的行 是灰色的 。
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -62,8 +68,13 @@ class TodoListViewController: UITableViewController {
         let alert =  UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: UIAlertControllerStyle.alert)
         let leftAction =  UIAlertAction(title: "Add Item", style: UIAlertActionStyle.default) { (action) in
             // what will happen once the user clicks the Add Item button on our UIAlert
-            if let item = textField.text {
-                self.itemArray.append(item)
+            if let itemTitle = textField.text {
+                let newItem : Item = Item()
+                newItem.title = itemTitle
+                self.itemArray.append(newItem)
+                //TODO: - 存储数据到用户偏好设置: value 可以为任意类型的数据(字符串、字典。。。)
+                self.defaults.set(self.itemArray, forKey: "TodoListArray")
+                
                 self.tableView.reloadData()
             }
         }
---
+++ Todoey/Data Modal/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Modal.swift
+//  Todoey
+//
+//  Created by 吴飞 on 2018/11/6.
+//  Copyright © 2018 吴飞. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
